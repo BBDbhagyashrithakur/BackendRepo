cb01ac26049c20a12c127adf0b209eeb9e626c7a
ritu <ritu.baviskar@bbd.co.za>
2024-05-10 18:53:08 +0530

    new changes

--- src/main/java/com/example/ATCProject/Repository/JobPostRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.ATCProject.Repository;
-
-import com.example.ATCProject.Entity.JobPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobPostRepo extends JpaRepository<JobPost,Integer> {
-}
